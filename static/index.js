3263c4687c7b2a9a3be7e8f0b2557dcead9a6217
Hurricanexq <93673721+Hurricanexq@users.noreply.github.com>
2022-01-30 13:11:39 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -441,11 +441,11 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '试着好好练一下？';
-        if (cps <= 8) return 'TCL';
-        if (cps <= 10)  return 'TQL';
-        if (cps <= 15) return '您';
-        return '人？';
+        if (cps <= 5) return '小舔一口堡';
+        if (cps <= 8) return '吔！';
+        if (cps <= 10)  return '马 克 6 专 精';
+        if (cps <= 15) return '您就是吔堡奇人？';
+        return '堡都让你吔绝了';
     }
 
     function toStr(obj) {
